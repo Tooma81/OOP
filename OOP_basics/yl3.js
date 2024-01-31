8683a89c86df9c8528469f2d35a9cdc4891051d0
tooma81 <toomas.kaarel@gmail.com>
2024-01-31 11:18:24 +0200

    OOP basics exrcise 3

---
+++ OOP_basics/yl3.js
@@ -0,0 +1,34 @@
+class Student {
+    #id;
+    #name;
+    #status;
+
+    constructor(id, name, status = 'Active') {
+        this.#id = id;
+        this.#name = name;
+    } 
+
+    getId() {
+        return this.#id
+    } 
+
+    getName() {
+        return this.#name
+    }
+    
+    getStatus() {
+        return this.#status
+    } 
+
+    setName(name) {
+        this.#name = name
+    } 
+
+    setName(status) {
+        this.#status = status
+    } 
+
+    student1 = new Student(this.#id, this.#name)
+    console.log(student1)
+} 
+
